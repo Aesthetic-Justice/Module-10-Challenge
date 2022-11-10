a3040aaf93b617b338ebaf7bde052b9123ad0c94
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2022-11-09 22:59:03 -0500

    Initial Test working

---
+++ tests/Employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require(`../lib/Employee`);
+
+const employeeTest = new Employee(`John`,`117`,`JohnHalo@UNSC.gov`);
+
+test(`returns name`,() => {
+    expect(employeeTest.getName()).toBe(`John`);
+})
+
+test(`returns id`,() => {
+    expect(employeeTest.getId()).toBe(`117`);
+})
+
+test(`returns email`,() =>{
+    expect(employeeTest.getEmail()).toBe(`JohnHalo@UNSC.gov`);
+})
