8ff26e954d5e277c3cdc238d102beec6fd5dd462
Aesthetic-Justice <96902541+Aesthetic-Justice@users.noreply.github.com>
2022-11-09 23:09:05 -0500

    Engineer test working

---
+++ tests/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require(`../lib/Engineer`);
+
+const testEngineer = new Engineer(`Engi`,`2`,`Engineer@2Fort.blu`,`EngineerGit`);
+
+test(`returns name`,() => {
+    expect(testEngineer.getName()).toBe(`Engi`);
+})
+
+test(`returns id`,() => {
+    expect(testEngineer.getId()).toBe(`2`);
+})
+
+test(`returns email`,() =>{
+    expect(testEngineer.getEmail()).toBe(`Engineer@2Fort.blu`);
+})
+
+test(`returns github`,() =>{
+    expect(testEngineer.getGithub()).toBe(`EngineerGit`);
+})
